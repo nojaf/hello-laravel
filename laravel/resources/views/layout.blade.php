e6fb0f18f1f5e77af0d63db55cd8089455512daa
Florian Verdonck <Florian.Verdonck@wkfs-frc.local>
2016-08-19 12:58:56 +0200

    Handling css & js

--- laravel/resources/views/layout.blade.php
+++ laravel/resources/views/layout.blade.php
@@ -3,41 +3,9 @@
     <head>
         <title>Laravel</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-
-            main{
-                padding:0 15px;
-            }
-        </style>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css"> 
+        <link rel="stylesheet" type="text/css" href="{{ elixir('css/app.css')  }}">
+        <link rel="stylesheet" type="text/css" href="/css/style.css" />
     </head>
     <body>
         <main>
