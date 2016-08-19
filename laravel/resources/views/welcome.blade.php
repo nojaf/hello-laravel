58700a34dc82f50d1b091c4601f6d01552eadc05
Florian Verdonck <Florian.Verdonck@wkfs-frc.local>
2016-08-19 12:38:53 +0200

    Added layout page

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -1,53 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+@extends('layout')
+@section('content')
+    @if(empty($people))
+    <p>No people in array</p>
+    @endif
+    
+    @unless(empty($people))
+    <p>There are people</p>
+    @endunless
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        @if(empty($people))
-        <p>No people in array</p>
-        @endif
-        
-        @unless(empty($people))
-        <p>There are people</p>
-        @endunless
-
-        <ul>
-        @foreach ($people as $person)
-            <li>{{ $person }}.</li>    
-        @endforeach
-        </ul>
-    </body>
-</html>
+    <ul>
+    @foreach ($people as $person)
+        <li>{{ $person }}.</li>    
+    @endforeach
+    </ul>
+@stop
